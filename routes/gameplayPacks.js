8fb49e8ccc69df9bfa801fea5dddf0a648f65e25
Joshua Vaughan <joshcv2112@gmail.com>
2023-12-03 22:47:25 -0800

    created PUT gameplay-packs endpoint

--- routes/gameplayPacks.js
+++ routes/gameplayPacks.js
@@ -15,10 +15,23 @@ router.get('/', async function(req, res, next) {
 /* POST gameplay packs */
 router.post('/', async function(req, res, next) {
   res.send("POST Request Called!");
+  
   try { 
     res.json(await gameplayPacks.writeNewGameplayPack(req.body));
   } catch (err) {
     console.error(`Error while writing new 'gameplay pack' record`, err.message);
+    next(err);
+  }
+});
+
+/* PUT gameplay packs*/
+router.put('/', async function(req, res, next) {
+  res.send("PUT Request Called!");
+  
+  try { 
+    res.json(await gameplayPacks.updateGameplayPack(req.body));
+  } catch (err) {
+    console.error(`Error while updating 'gameplay pack' record`, err.message);
     next(err);
   }
 });
--- services/gameplayPacks.js
+++ services/gameplayPacks.js
@@ -17,15 +17,53 @@ async function getMultiple(page = 1){
   }
 }
 
+/**
+ * Takes an array of objects from the request's JSON body 
+ * and writes all objects to the gameplayPacks DB table
+ * @param {Array} body 
+ */
 async function writeNewGameplayPack(body) {
   console.log("CREATING NEW RECORD");
-  await db.query(`INSERT INTO \`tswmaps2_svelte_dev\`.\`gameplayPacks\`
-  (\`acronym\`, \`name\`, \`nameShort\`, \`nameLong\`, \`nameAlternate\`, \`locale\`, \`releaseDate\`,\`country\`,\`developer\`,\`era\`,\`powerType\`,\`tsw1\`,\`tsw2\`,\`tsw3\`,\`tsw4\`)
-  VALUES
-  ("${body.acronym}","${body.name}","${body.nameShort}","${body.nameLong}","${body.nameAlternate}","${body.locale}","${body.releaseDate}","${body.country}","${body.developer}","${body.era}","${body.powerType}","${body.tsw1}","${body.tsw2}","${body.tsw3}","${body.tsw4}");`);
+
+  // build query string
+  let query = `INSERT INTO \`tswmaps2_svelte_dev\`.\`gameplayPacks\`
+  (\`acronym\`, \`name\`, \`nameShort\`, \`nameLong\`, \`nameAlternate\`, \`locale\`, \`releaseDate\`,\`country\`,\`developer\`,\`era\`,\`powerType\`,\`tsw1\`,\`tsw2\`,\`tsw3\`,\`tsw4\`) VALUES `;
+  
+  // add each object to the INSERT query
+  body.forEach(element => {
+    console.log(element);
+    query += `("${element.acronym}","${element.name}","${element.nameShort}","${element.nameLong}","${element.nameAlternate}","${element.locale}","${element.releaseDate}","${element.country}","${element.developer}","${element.era}","${element.powerType}","${element.tsw1}","${element.tsw2}","${element.tsw3}","${element.tsw4}"),`;
+  });
+  
+  // replace last char of query with a ';' then run INSERT query
+  query = query.replace(/.$/,";");
+  await db.query(query);
+}
+
+async function updateGameplayPack(body) {
+  console.log('UPDATING EXISTING RECORD');
+
+  // define sql query string
+  let query = `UPDATE \`tswmaps2_svelte_dev\`.\`gameplayPacks\` SET `;
+
+  // Add each property in the request body to the query (except id)
+  for (const property in body) {
+    if (property !== 'id') {
+      query += `\`${property}\` = "${body[property]}" ,`;
+    }
+  }
+
+  // remove the trailing ','
+  query = query.replace(/.$/,"");
+  
+  // add WHERE condition then run UPDATE query
+  query += `WHERE \`id\` = ${body.id};`;
+  console.log(`\n${query}\n`);
+  await db.query(query);
 }
 
 module.exports = {
   getMultiple,
-  writeNewGameplayPack
+  writeNewGameplayPack,
+  updateGameplayPack
 }
